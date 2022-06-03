973c9dbbd9460722909ee68975c773dad87cb03a
Osman Jalloh <osmanj@netapp.com>
2022-06-03 10:24:27 -0700

    Beginning work to inplement an ansible based operator for automation Jannah.io Infrastructure

---
+++ ansible/library/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+# :maintainer:    Osman Jalloh <Os@jallohmedia>
+# :maturity:      new
+# :depends:       xxxxxx
+# :platform:      all
+
+from __future__ import absolute_import
+from __future__ import print_function
